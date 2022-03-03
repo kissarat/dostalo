ed6f63f46f8291a5b88bc5ec972aed401729d755
Taras Labiak <kissarat@gmail.com>
2022-03-03 14:48:02 +0200

    + configuration from CLI options

---
+++ config.js
@@ -0,0 +1,30 @@
+const { dirname, join } = require('path')
+const { tmpdir } = require('os')
+const { readJSON, getOptions } = require('./utils')
+const { omit, pick } = require('rambda')
+
+const config = readJSON(join(__dirname, 'default-config.json'));
+const currentdir = process.cwd()
+
+const [options] = getOptions(process.argv)
+
+try {
+    const headers = config.headers
+    const local = readJSON(options.config || join(currentdir, 'local-config.json'))
+    Object.assign(config, omit(["headers"], local))
+    if (local.headers) {
+        config.headers = Object.assign(headers, local.headers)
+    }
+} catch (err) {
+    console.warn("No local configuration")
+}
+
+config.basedir = __dirname
+config.currentdir = currentdir
+Object.assign(config, pick(['targets', 'proxies', 'tmpdir'], options))
+
+if ('string' !== typeof config.tmpdir) {
+    config.tmpdir = tmpdir()
+}
+
+module.exports = config
--- utils.js
+++ utils.js
@@ -1,7 +1,30 @@
-const { readFileSync } = require("fs")
+const { readFileSync } = require("fs");
+const { basename } = require("path");
 
 function readJSON(filename) {
     return JSON.parse(readFileSync(filename))
 }
 
-module.exports = { readJSON }
+function getProcessArguments(args = process.argv) {
+    // const nodeIndex = isElectronNode ? 0 : args.map(a => basename(a)).indexOf('node');
+    const nodeIndex = args.map(a => basename(a)).indexOf('node');
+    if (nodeIndex < 0) {
+        throw new Error(`Program node not found at "${args.join(' ')}"`)
+    }
+    return args.slice(nodeIndex + 2);
+}
+
+function getOptions(args = process.argv, options = {}) {
+    const ordered = [];
+    for (const arg of getProcessArguments(args, options)) {
+        if (arg.indexOf("--") === 0) {
+            const p = arg.split("=");
+            options[p[0].slice(2)] = 2 === p.length ? p[1] : true;
+        } else {
+            ordered.push(arg);
+        }
+    }
+    return [options, ...ordered];
+}
+
+module.exports = { readJSON, getProcessArguments, getOptions }
